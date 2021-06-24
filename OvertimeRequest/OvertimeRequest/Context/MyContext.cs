89d7713892c641fafaa877af641090705db6d468
Farhan Evirzal <farhanevirzal15@gmail.com>
2021-06-24 10:45:14 +0700

    MyContext

---
+++ OvertimeRequest/OvertimeRequest/Context/MyContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OvertimeRequest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OvertimeRequest.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext()
+        {
+        }
+
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AccountRole> AccountRoles { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<OvertimeApply> OvertimeApplies { get; set; }
+        public DbSet<OvertimeApplyEmployee> overtimeApplyEmployees { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+    }
+}
