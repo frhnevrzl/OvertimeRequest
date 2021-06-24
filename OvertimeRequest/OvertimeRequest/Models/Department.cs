9234c462c776ed3b32fa10c3691493501d2fb6de
Putri Ayu Pratiwi <utimarutiii@gmail.com>
2021-06-24 10:22:16 +0700

    update models
    department, overtimeapply

---
+++ OvertimeRequest/OvertimeRequest/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OvertimeRequest.Models
+{
+    [Table("TB_M_Department")]
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public string DepartmentLoc { get; set; }
+    }
+}
---
+++ OvertimeRequest/OvertimeRequest/Models/OvertimeApply.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OvertimeRequest.Models
+{
+    [Table("TB_M_OvertimeApply")]
+    public class OvertimeApply
+    {
+        [Key]
+        public int OvertimeId { get; set; }
+        public string OvertimeName { get; set; }
+        public DateTime SubmissionDate { get; set; }
+
+    }
+}
