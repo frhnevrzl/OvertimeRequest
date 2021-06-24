e876ce5c4a68a0dfa38a942ac943696ad555a2e6
Farhan Evirzal <farhanevirzal15@gmail.com>
2021-06-24 10:09:50 +0700

    Employee.cs

---
+++ OvertimeRequest/OvertimeRequest/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OvertimeRequest.Models
+{
+    public class Employee
+    {
+        public int NIP { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+        public string Religion { get; set; }
+        public int Salary { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int ManagerId { get; set; }
+
+    }
+}
