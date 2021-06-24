30cc5a279e6eb968e9194f7e00baf8ce5950ddc4
Putri Ayu Pratiwi <utimarutiii@gmail.com>
2021-06-24 10:07:50 +0700

    update models
    accountrole, account, role

---
+++ OvertimeRequest/OvertimeRequest/Models/AccountRole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OvertimeRequest.Models
+{
+    [Table("TB_TR_AccountRole")]
+    public class AccountRole
+    {
+        public int AccountId { get; set; }
+        public int RoleId { get; set; }
+        public virtual Role Role { get; set; }
+        public virtual Account Account { get; set; }
+    }
+}
