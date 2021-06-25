6f58dd3a9dbd4a36ace6c249f864ec18ab5e1a75
Putri Ayu Pratiwi <utimarutiii@gmail.com>
2021-06-25 10:50:32 +0700

    Update OvertimeRequestViewModel
    OvertimeRequestViewModel

---
+++ OvertimeRequest/OvertimeRequest/ViewModels/OvertimeFormVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OvertimeRequest.ViewModels
+{
+    public class OvertimeFormVM
+    {
+        public int OvertimeId { get; set; }
+        public string OvertimeName { get; set; }
+        public DateTime SubmissionDate { get; set; }
+        public int NIP { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int AccountId { get; set; }
+        public int RoleId { get; set; }
+    }
+}
