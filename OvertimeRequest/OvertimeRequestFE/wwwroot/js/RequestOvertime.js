23aa2905b439a29f80afaea7b87f94106fe2a9d9
Farhan Evirzal <farhanevirzal15@gmail.com>
2021-06-29 15:06:46 +0700

    frontend request overtime

--- OvertimeRequest/OvertimeRequestFE/wwwroot/js/RequestOvertime.js
+++ OvertimeRequest/OvertimeRequestFE/wwwroot/js/RequestOvertime.js
@@ -1,72 +1,32 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
-    var date = new Date();
-    var d = date.getDate();
-    var m = date.getMonth();
-    var y = date.getFullYear();
+﻿////function approveOvertimeRequestManager() {
+////    var object = new Object();
+////    var Nip = $("#NIP").val();
+////    var idOvertime = $("#idOvertime").val();
+////    object.NIP = Nip;
+////    object.Status = 1;
+////    object.overtimeApplyId = idOvertime;
+////}
 
-    var calendarEl = document.getElementById("calendar");
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        plugins: ["interaction", "dayGrid", "timeGrid", "resourceTimeline"],
-        header: {
-            left: "prev,next today",
-            center: "title",
-            right: "dayGridMonth,timeGridWeek,timeGridDay"
-        },
-        editable: true,
-        events: [
-            {
-                title: "Sales Meeting",
-                start: new Date(y, m, 2, 10, 30),
-                end: new Date(y, m, 2, 11, 30),
-                allDay: false
-            },
-            {
-                title: "Marketing Meeting",
-                start: new Date(y, m, 3, 11, 30),
-                end: new Date(y, m, 3, 12, 30),
-                allDay: false
-            },
-            {
-                title: "Production Meeting",
-                start: new Date(y, m, 4, 15, 30),
-                end: new Date(y, m, 4, 16, 30),
-                allDay: false
-            }
-        ]
-    });
-
-    calendar.render();
-});
-
-function approveOvertimeRequestManager() {
-    var object = new Object();
-    var Nip = $("#NIP").val();
-    var idOvertime = $("#idOvertime").val();
-    object.NIP = Nip;
-    object.Status = 1;
-    object.overtimeApplyId = idOvertime;
-}
-
-function approveOvertimeRequestFinance() {
-    var object = new Object();
-    var Nip = $("#NIP").val();
-    var idOvertime = $("#idOvertime").val();
-    object.NIP = Nip;
-    object.Status = 2;
-    object.overtimeApplyId = idOvertime;
-}
-//function RejcetOvertimeRequest() {
-//    var Nip = $("#NIP").val();
-//    if ()
-//}
-    $(document).ready(function(){
-      var date_input=$('input[name="date"]'); //our date input has the name "date"
-      var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
-      var options={
-        format: 'mm/dd/yyyy',
-        container: container,
-        todayHighlight: true,
-        autoclose: true,
-      };
-      date_input.datepicker(options);
-    })
+////function approveOvertimeRequestFinance() {
+////    var object = new Object();
+////    var Nip = $("#NIP").val();
+////    var idOvertime = $("#idOvertime").val();
+////    object.NIP = Nip;
+////    object.Status = 2;
+////    object.overtimeApplyId = idOvertime;
+////}
+//////function RejcetOvertimeRequest() {
+//////    var Nip = $("#NIP").val();
+//////    if ()
+//////}
+////    $(document).ready(function(){
+////      var date_input=$('input[name="date"]'); //our date input has the name "date"
+////      var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+////      var options={
+////        format: 'mm/dd/yyyy',
+////        container: container,
+////        todayHighlight: true,
+////        autoclose: true,
+////      };
+////      date_input.datepicker(options);
+////    })
