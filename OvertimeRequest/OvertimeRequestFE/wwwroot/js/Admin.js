6fd8623a8c750cb94ac45f32f5e21b912e94efcb
Farhan Evirzal <farhanevirzal15@gmail.com>
2021-07-07 16:53:19 +0700

    r

--- OvertimeRequest/OvertimeRequestFE/wwwroot/js/Admin.js
+++ OvertimeRequest/OvertimeRequestFE/wwwroot/js/Admin.js
@@ -2,20 +2,53 @@
 var stringnip = $("#nip").val();
 
 $.getJSON(url, function (response) {
-/*    console.log(response);*/
+    console.log(response);
     var data = response.map(item => item.roleName).filter((value, index, self) => self.indexOf(value) === index);
-    //console.log(data);
+    console.log(data);
     var dataCount = response.map(item => item.accountRoles.length);
-    //console.log(dataCount);
+    console.log(dataCount);
+    var options = {
+        series: [{
+            data: dataCount
+        }],
+        chart: {
+            type: 'bar',
+            height: 350
+        },
+        plotOptions: {
+            bar: {
+                borderRadius: 4,
+                horizontal: true,
+            }
+        },
+        dataLabels: {
+            enabled: true
+        },
+        xaxis: {
+            categories: data,
+        }
+    };
+
+    var chart = new ApexCharts(document.querySelector("#UniCh"), options);
+    chart.render();
+});
+
+var url2 = 'https://localhost:44364/API/overtimeapply/'
+$.getJSON(url2, function (response) {
+    console.log(response);
+    var data = response.map(item => item.overtimeApplyEmployees.status).filter((value, index, self) => self.indexOf(value) === index);
+    console.log(data);
+    var dataCount = response.map(item => item.overtimeApplyEmployees.length);
+    console.log(dataCount);
 
     var options = {
         series: dataCount,
         chart: {
-            type: 'pie'
+            type: 'donut'
         },
         labels: data
     };
 
-    var chart = new ApexCharts(document.querySelector("#UniCh"), options);
+    var chart = new ApexCharts(document.querySelector("#ReqCh"), options);
     chart.render();
 });
